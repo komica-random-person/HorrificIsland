83225cb2bfcefa57ae38a87c9fd8bd6583e8871e
komica <komica-random-person@users.noreply.github.com>
2019-04-23 13:30:54 +0800

    update: detection of reference

--- app/script/global.babel.js
+++ app/script/global.babel.js
@@ -1,7 +1,7 @@
 'use strict';
 const getID = id => document.getElementById(id);
-const getQuery = css => document.querySelector(css);
-const getQueries = css => document.querySelectorAll(css);
+const getQuery = (css, ele=document) => ele.querySelector(css);
+const getQueries = (css, ele=document) => ele.querySelectorAll(css);
 const getQueriesArray = css => Array.prototype.slice.apply(getQueries(css));
 const escape = s => s.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
 
@@ -9,14 +9,25 @@ $(() => {
   /* hover box */
   const hoverbox = new HoverBox();
   const updateRefNum = () => {
+    /* Scan every existing content and replace ">>\d{8}" with span.quote */
     $('p.content').each((_, p) => {
-      if(p.innerText.match(/>>\d{8}\s/) !== null){
-        const match = p.innerText.match(/(>>\d{8})\s/)[1];
-        p.innerHTML = p.innerHTML.split(escape(match)).join(`<span class="quote" data-quoteType="num" data-num="${match.slice(2)}">${escape(match)}</span>`);
+      if(p.innerText.match(/>>\d{8}\s*/) !== null) {
+        const thread = p.parentElement.parentElement;
+        const match = p.innerText.match(/>>\d{8}\s/g);
+        match.forEach(_match => {
+          /* Since split can't detect \n, slice the \n */
+          _match = _match.slice(0, _match.length - 1);
+          const num = _match.slice(2);
+          if(getQuery(`.quotable[data-num="${num}"]`, thread) !== null)
+            p.innerHTML = p.innerHTML.split(escape(_match)).join(`<span class="quote" data-quoteType="num" data-num="${num}">${escape(_match)}</span>`);
+          else
+            p.innerHTML = p.innerHTML.split(escape(_match)).join(`<span class="quote missing" data-quoteType="num" data-num="${num}">${escape(_match)}</span>`);
+        });
       }
     });
   };
   updateRefNum();
+
 });
 
 class HoverBox {
