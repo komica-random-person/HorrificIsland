9c8cce00420c9c6d7855fa70c3fa4f3e9cfa93ad
komica <komica-random-person@users.noreply.github.com>
2019-04-30 21:08:42 +0800

    local api url

--- app/script/global.babel.js
+++ app/script/global.babel.js
@@ -9,6 +9,7 @@ const findParent = (element, pattern) => {
     element = element.parentElement;
   return element.parentElement === null ? null : element;
 };
+const apiUrl = 'http://localhost:8888';
 
 const showImg = (imgContainer) => {
   /* This function is for onclick event on IMG tag.
