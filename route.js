3e198af5eeb9a4f7ff6c8963ec61f941d55f7eb4
komica <komica-random-person@users.noreply.github.com>
2019-06-09 21:19:46 +0800

    修正回復模式沒有預載主題

--- route.js
+++ route.js
@@ -1,18 +1,14 @@
 /* global process, __dirname */
 module.exports = app => {
-  app.get('^/$', (req, res) => {
+  const getThreadPageInfo = (req, { name='index', title='Index', description='index', pageContent={} }) => {
     let themeIndex = req.cookies.theme === undefined ? 0 : Number(req.cookies.theme);
     themeIndex = isNaN(themeIndex) ? 0 : themeIndex;
+    pageContent.themeIndex = themeIndex;
+    return { name, title, description, pageContent };
+  };
+  app.get('^/$', (req, res) => {
     req.app.get('getMainContent')(content => {
-      const pageInfo = {
-        name: 'index',
-        title: 'Index',
-        description: 'index',
-        pageContent: {
-          komica: content.content,
-          themeIndex,
-        }
-      };
+      const pageInfo = getThreadPageInfo(req, { pageContent: { komica: content.content }});
       const userid = req.cookies.keygen;
       req.app.get('getHIContent')(userid, result => {
         if(result.res === 200)
@@ -28,15 +24,7 @@ module.exports = app => {
     const articleNum = req.params.num;
     api.threadAPI(`thread/${articleNum}`, req.cookies.keygen, result => {
       result = JSON.parse(result);
-      const pageInfo = {
-        name: 'index',
-        title: result.post.content,
-        description: 'index',
-        pageContent: {
-          hisland: [result],
-          replyFormat: true
-        },
-      };
+      const pageInfo = getThreadPageInfo(req, { pageContent: { hisland: [result], replyFormat: true } });
       const renderedContent = req.app.get('render')(pageInfo);
       res.send(renderedContent);
     }, error => {
